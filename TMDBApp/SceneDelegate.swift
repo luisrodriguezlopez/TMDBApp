6191582f8f2400dfd1e82ef7fefd0d37693be9c7
luisrodriguezlopez <luuiz.rodriguez.lopez@gmail.com>
2024-08-15 14:35:36 -0700

    generar tambien entrada desde scenenDelegate

--- TMDBApp/SceneDelegate.swift
+++ TMDBApp/SceneDelegate.swift
@@ -16,7 +16,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+               window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+               window?.windowScene = windowScene
+               var vc = ViewController()
+               vc.loader = RemoteWithLocalFallBackDataLoader(remoteMovies: RemoteLoader(), localMovies: LocalLoader())
+               window?.rootViewController = vc
+               window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -52,4 +59,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
+
 
