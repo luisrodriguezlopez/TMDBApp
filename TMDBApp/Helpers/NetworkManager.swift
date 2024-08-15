1949d5964b6febd87c39d288ad51118d81dcaa00
luisrodriguezlopez <luuiz.rodriguez.lopez@gmail.com>
2024-08-15 14:38:07 -0700

    agregar clase para verificar status de la red

---
+++ TMDBApp/Helpers/NetworkManager.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkMonitor.swift
+//  TMDBApp
+//
+//  Created by luis rodriguez on 14/08/24.
+//
+
+import Network
+
+class NetworkManager {
+    private var isConnectedMsg:String = ""
+    private var image:String = ""
+    private var connection : Bool = false
+    
+    func checkInternetConnectionDevice() -> String {
+        
+        let monitor = NWPathMonitor()
+        monitor.pathUpdateHandler = { path in
+            let connectivity = path.status == .satisfied
+            self.isConnectedMsg   =  connectivity ?  "Connected to the internet" : "No internet connection"
+            self.image =  connectivity  ? "network" : "network.slash"
+                
+        }
+        let queue = DispatchQueue(label: "NetworkMonitorMessage")
+        monitor.start(queue: queue)
+        return self.isConnectedMsg
+    }
+    
+    func checkInternetConnection(completion: @escaping(Bool) -> Void) {
+        let monitor = NWPathMonitor()
+        monitor.pathUpdateHandler = { path  in
+          completion(path.status == .satisfied ? true : false)
+        }
+        
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+    }
+    
+    func getImage() -> String {
+        return self.image
+    }
+    
+}
+
