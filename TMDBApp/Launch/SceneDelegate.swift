01e4a13aa0892101025cca94f01c05d7cebe343c
luisrodriguezlopez <luuiz.rodriguez.lopez@gmail.com>
2024-08-16 19:27:07 -0700

    appdelegate para pruebas, inicializar modulo de pelicula en scenedelegate y appdelegate

--- TMDBApp/Launch/SceneDelegate.swift
+++ TMDBApp/Launch/SceneDelegate.swift
@@ -17,11 +17,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-               window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-               window?.windowScene = windowScene
-               var vc = ViewController()
-               vc.loader = RemoteWithLocalFallBackDataLoader(remoteMovies: RemoteLoader(), localMovies: LocalLoader())
-               window?.rootViewController = vc
+        
+          var vc = MoviesViewController()
+             //  vc.loader = RemoteWithLocalFallBackDataLoader(remoteMovies: RemoteLoader(), localMovies: LocalLoader())
+                vc.worker = MoviesViewWorker()
+                vc.worker?.localMovies = LocalWorker()
+                vc.worker?.remoteMovies = RemoteWorker()
+                vc.remoteWorker = RemoteWorker()
+                vc.localWorker = LocalWorker()
+
+                window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+                window?.rootViewController = vc
+                window?.windowScene = windowScene
                window?.makeKeyAndVisible()
         
     }
